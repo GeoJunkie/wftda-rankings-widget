cc950cba9aaaf2fc5e81876633dfb9734b4ad4c9
Mike Straw <mike.straw@automattic.com>
2023-09-28 11:18:50 -0400

    Update version and contact info

--- league-wftda-ranking.php
+++ league-wftda-ranking.php
@@ -7,11 +7,11 @@
  *
  * @wordpress-plugin
  * Plugin Name:       WFTDA Rankings Widget
- * Plugin URI:        https://github.com/GeoJunkie/league-wftda-ranking
+ * Plugin URI:        https://wordpress.org/plugins/wftda-rankings-widget/
  * Description:       A widget to show a WFTDA league's ranking information in a widget.
- * Version:           1.0.2
+ * Version:           1.1.0
  * Author:            Mike Straw (aka Stray Taco)
- * Author URI:        http://bit.ly/Stray_Taco
+ * Author URI:        https://github.com/GeoJunkie/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       league-wftda-ranking
@@ -27,7 +27,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Current plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  */
-define( 'LEAGUE_WFTDA_RANKING_VERSION', '1.0.2' );
+define( 'LEAGUE_WFTDA_RANKING_VERSION', '1.1.0' );
 
 /**
  * Plugin constants
